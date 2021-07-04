f331bea3736b4171dc123560aeb7e09a6676c77d
70726 <70726@DESKTOP-5ED4BIC.mshome.net>
2021-07-04 17:36:25 -0300

    AULA 04/07/2021

---
+++ Aula1/src/aula1/MainCarroMVC.java
@@ -0,0 +1,21 @@
+package aula1;
+import aula1.view.BoletimMVC;
+
+public class MainCarroMVC {
+
+    public static void main(String[] args) {
+        new BoletimMVC().setVisible(true);
+        /*
+        CRIAR FORMULARIO PARA INSERIR:
+            PLACA,
+            MODELO,
+            MARCA,
+            KM,
+            QUANTOS LITROS ABASTECEU,
+            CAPACIDADE DO TANQUE.
+        
+        LISTAR OS DADOS EM UMA TABELA MOSTRANDO A AUTONOMIA DO CARRO.
+        
+        */
+    }
+}
